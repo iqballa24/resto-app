d0e9928fe02ff3e21127f601a21ba363d3c86f0c
Iqbal Nugraha <iqbalnugraha347@gmail.com>
2022-12-31 18:50:45 +0700

    initialize commit

---
+++ webpack.prod.js
@@ -0,0 +1,33 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const path = require('path');
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  output: {
+    filename: 'js/[name]-[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'img/[name]-[hash][ext]',
+    clean: true,
+  },
+  optimization: {
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.squooshMinify,
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style/style-[contenthash].css',
+    }),
+  ],
+});
